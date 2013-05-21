9f0e26f1c267c2c5601953953a3b746e0142508f
Jimmy Zimmerman <jimmy.zimmerman@gmail.com>
2013-05-21 06:18:47 -0600

    Create the gem.

---
+++ lib/familysearch/gedcomx.rb
@@ -0,0 +1,7 @@
+require "familysearch/gedcomx/version"
+
+module FamilySearch
+  module Gedcomx
+    # Your code goes here...
+  end
+end
---
+++ lib/familysearch/gedcomx/version.rb
@@ -0,0 +1,5 @@
+module FamilySearch
+  module Gedcomx
+    VERSION = "0.0.1"
+  end
+end
